ad2dc328b2c5bad79e842f3b27058606f2c1559c
Ivan Yaremko <yaremich98@gmail.com>
2023-03-18 10:32:22 +0200

    (67) React. AJAX

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import React from 'react';
+import UsersLoader from './components/UsersLoader';
+
+function App() {
+    return <>
+      <UsersLoader />
+    </>
+}
+
+export default App;
